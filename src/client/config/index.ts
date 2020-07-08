9d4c2428c52f623a0c80d79504c8b6baba80100b
xianengqi <xianengqi@gmail.com>
2020-07-08 23:02:21 +0800

    把不需要变动的字段放在constans里

---
+++ src/client/config/index.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import ApolloClient from 'apollo-boost';
+
+// 默认为当前域名
+const serverDomain = '';
+
+function getGraphqlClient(): ApolloClient<{}> {
+  return new ApolloClient({
+    uri: `${serverDomain}/graphql`
+  });
+}
+
+const config = {
+  serverDomain,
+  getGraphqlClient
+};
+
+export default config;
---
+++ src/client/constants/index.ts
@@ -0,0 +1,19 @@
+export const HOUSE_NUMBER = '房源数';
+export const BUILDER_NUMBER = '楼盘数';
+export const SALE_TIMES = '开盘数';
+export const AREA = '区域';
+export const GITHUB_URL = 'https://github.com/xianengqi/whfang-spider';
+export const COPYRIGHT = 'Copyright 2020 XiaNengQi. All Rights Reserved';
+export const BEIAN_ICP = '鄂ICP备17036475号-2';
+export const RISE_COLOR = '#5eba00';
+export const DECLINE_COLOR = '#cd201f';
+export const LOADING_TIP = '页面加载中...';
+export const HOUSE_PURCHASE_REGISTARTION =
+  'http://zw.cdzj.chengdu.gov.cn/lottery/accept/index';
+
+export const tabKeyRouterMap: { [x: string]: string } = {
+  '2019': '/2019',
+  home: '/',
+  '/2019': '2019',
+  '/': 'home'
+}
