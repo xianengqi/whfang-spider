899e44ac6447a88ca54d35f56334c2efc97eace5
xianengqi <xianengqi@gmail.com>
2020-07-19 23:31:50 +0800

    feat: :dizzy:编写StatisticCard组件
    用来展示月份和季度的对比

--- src/client/containers/Home/index.tsx
+++ src/client/containers/Home/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { RouteComponentProps } from 'react-router';
 import { Layout } from 'antd';
 
+import StatisticCard from '../../componets/StatisticCard';
+
 const { lazy } = React;
 const { Content } = Layout;
 const CurrentHouse = lazy(() => import('../../componets/CurrentHouse'));
@@ -11,6 +13,9 @@ const Home: React.FC<RouteComponentProps> = () => {
     <Content className="content">
       <div className="content-section">
         <CurrentHouse />
+      </div>
+      <div className="content-statistic-card">
+        <StatisticCard />
       </div>
     </Content>
   );
