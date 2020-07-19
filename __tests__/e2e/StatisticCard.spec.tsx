9fad6c447c4a436850f7088d9491e42f54029729
xianengqi <xianengqi@gmail.com>
2020-07-19 23:47:27 +0800

    test: :white_check_mark:编写StatisticCard组件的测试用例

---
+++ __tests__/e2e/StatisticCard.spec.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, RenderResult } from '@testing-library/react';
+import { AppContext, globalData } from '../../src/client/context/appContext';
+import StatisticCard from '../../src/client/componets/StatisticCard';
+import { mockHouse } from '../../__mocks__/db';
+
+const data = { ...globalData, allData: mockHouse };
+
+let wrapper: RenderResult;
+describe('StatisticCard 组件', () => {
+  beforeEach(() => {
+    wrapper = render(
+      <AppContext.Provider value={data}>
+        <div>
+          <StatisticCard />
+        </div>
+      </AppContext.Provider>
+    );
+  });
+  it('是否渲染成功 ?', () => {
+    expect(wrapper.container.querySelectorAll('.ant-card').length).toBe(4);
+  })
+})
