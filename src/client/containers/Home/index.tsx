3b93b59ec931842d9846a5cbda14af79a6feff00
xianengqi <xianengqi@gmail.com>
2020-07-21 03:27:14 +0800

    feat: :bento:编写统计图

--- src/client/containers/Home/index.tsx
+++ src/client/containers/Home/index.tsx
@@ -1,14 +1,75 @@
 import React from 'react';
+import _ from 'lodash';
+import dayjs from 'dayjs';
 import { RouteComponentProps } from 'react-router';
-import { Layout } from 'antd';
+import { Layout, Col, Row, Tabs } from 'antd';
 
+import utils from '../../utils';
 import StatisticCard from '../../components/StatisticCard';
+import BasicAreaGraph from '../../components/BasicAreaGraph';
+import Rank from '../../components/Rank';
 
-const { lazy } = React;
+import { AppContext } from '../../context/appContext';
+import * as constants from '../../constants';
+import './style.less';
+
+const { lazy, useContext } = React;
+const { TabPane } = Tabs;
 const { Content } = Layout;
 const CurrentHouse = lazy(() => import('../../components/CurrentHouse'));
 
+interface ImonthHouse {
+  month: string;
+  [constants.HOUSE_NUMBER]: number;
+}
+
+interface ImonthBuilder {
+  month: string;
+  [constants.BUILDER_NUMBER]: number;
+}
+
 const Home: React.FC<RouteComponentProps> = () => {
+
+  const { allData } = useContext(AppContext);
+
+  // 构建区域图需要的数据
+  const arrayByDay = _.groupBy(allData, (item) => {
+    return dayjs(item.beginTime).format('YYYY-MM');
+  });
+
+  const houseData: ImonthHouse[] = [];
+  const builderData: ImonthBuilder[] = [];
+  Object.keys(arrayByDay)
+    .sort()
+    .forEach((key) => {
+      const houseNumber = _.sumBy(arrayByDay[key], 'number');
+      builderData.push({
+        month: key,
+        [constants.BUILDER_NUMBER]: arrayByDay[key].length,
+      });
+      houseData.push({
+        month: key,
+        [constants.HOUSE_NUMBER]: houseNumber,
+      });
+    });
+
+    // 构建排行数据
+    const builderRankData = builderData.map((item) => ({
+      _id: utils.getRandomId(),
+      name: item.month,
+      number: item[constants.BUILDER_NUMBER],
+    }));
+    const houseRankData = houseData.map((item) => ({
+      _id: utils.getRandomId(),
+      name: item.month,
+      number: item[constants.HOUSE_NUMBER],
+    }));
+
+    // 柱状图数据
+    const { chartHouseData, chartBuilderData } = utils.getBasicColumnGraphData(
+      allData
+    );
+
   return (
     <Content className="content">
       <div className="content-section">
@@ -16,6 +77,23 @@ const Home: React.FC<RouteComponentProps> = () => {
       </div>
       <div className="content-statistic-card">
         <StatisticCard />
+      </div>
+      <div className="home-content-houses">
+        <Tabs type="card">
+          <TabPane tab={constants.HOUSE_NUMBER} key="1">
+            <Row>
+              <Col span={18}>
+                <BasicAreaGraph
+                  data={houseData}
+                  title={constants.HOUSE_NUMBER}
+                />
+              </Col>
+              <Col span={6}>
+                <Rank data={houseRankData} title="月份" unit="套" />
+              </Col>
+            </Row>
+          </TabPane>
+        </Tabs>
       </div>
     </Content>
   );
