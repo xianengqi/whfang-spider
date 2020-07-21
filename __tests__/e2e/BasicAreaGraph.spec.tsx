0699f08bc73518e46d5d89cb6635552885e94f3c
xianengqi <xianengqi@gmail.com>
2020-07-21 22:15:17 +0800

    test: :white_check_mark:编写BAsicAreaGraph组件单元测试

--- __tests__/e2e/BasicAreaGraph.spec.tsx
+++ __tests__/e2e/BasicAreaGraph.spec.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, RenderResult } from '@testing-library/react';
-import BasicAreaGraph, { Iprops } from '../../src/client/components/BasicAreaGraph';
+import BasicAreaGraph, {
+  Iprops,
+} from '../../src/client/components/BasicAreaGraph';
 
 const props: Iprops = {
   title: '房源数',
